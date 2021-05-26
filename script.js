318f80ab9fb56f0c4fe129bd1bfd6ec67fd64cd3
unknown <marcodotr@gmail.coom>
2021-05-26 11:43:32 -0500

    added time blocks

---
+++ script.js
@@ -0,0 +1,5 @@
+// save button to add value of input to local.set storage
+// local strage.get text area
+// display date /moment
+//conditionally render colors for past present future 
+$("#currentDay").text(moment().format("MMM Do,YYYY, hh"))
