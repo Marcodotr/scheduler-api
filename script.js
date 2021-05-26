db9072cc7f6ff69cc9377fa469b2605420bfed8d
unknown <marcodotr@gmail.coom>
2021-05-26 12:14:35 -0500

    set text area to local storage get val

--- script.js
+++ script.js
@@ -3,3 +3,11 @@
 // display date /moment
 //conditionally render colors for past present future 
 $("#currentDay").text(moment().format("MMM Do,YYYY, hh"))
+
+$(".save-button").on("click",function() {
+    var divid = $(this).parent().attr("id")
+    var textvalue = $(this).siblings(".textValue").val()
+    localStorage.setItem(divid,textvalue)
+})
+
+$("#9 .textValue").val(localStorage.getItem("9"))
