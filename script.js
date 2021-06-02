6d6fdd9453dba3e679cfc4b5f01d9017cb457c12
unknown <marcodotr@gmail.coom>
2021-06-01 21:20:49 -0500

    past present future logic

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 // display date /moment
 //conditionally render colors for past present future 
 $("#currentDay").text(moment().format("MMM Do,YYYY, hh"))
-
+var currentTime = moment().hours()
 $(".save-button").on("click",function() {
     var divid = $(this).parent().attr("id")
     var textvalue = $(this).siblings(".textValue").val()
@@ -11,3 +11,29 @@ $(".save-button").on("click",function() {
 })
 
 $("#9 .textValue").val(localStorage.getItem("9"))
+$("#10 .textValue").val(localStorage.getItem("10"))
+$("#11 .textValue").val(localStorage.getItem("11"))
+$("#12 .textValue").val(localStorage.getItem("12"))
+$("#13 .textValue").val(localStorage.getItem("13"))
+$("#14 .textValue").val(localStorage.getItem("14"))
+$("#15 .textValue").val(localStorage.getItem("15"))
+$("#16 .textValue").val(localStorage.getItem("16"))
+$("#17 .textValue").val(localStorage.getItem("17"))
+
+function styleUpdate() {
+    $(".time-block").each(function(){
+      var divTime = $(this).attr("id")
+      if(divTime < currentTime) {
+          $(this).addClass("past")
+      }
+      else if (divTime == currentTime) {
+          $(this).removeClass("past")
+          $(this).addClass("present")
+      }
+      else if (divTime > currentTime) {
+          $(this).removeClass("past present")
+          $(this).addClass("future")
+      }
+    })
+}
+styleUpdate()
